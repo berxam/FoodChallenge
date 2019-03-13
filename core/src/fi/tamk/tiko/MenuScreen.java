9213a9ef2394dbf099ecc2f7df45753516eac6fa
Sami Bergman <sami.bergman@tuni.fi>
2019-03-13 17:23:47 +0200

    MenuScreen made great again.

--- core/src/fi/tamk/tiko/MenuScreen.java
+++ core/src/fi/tamk/tiko/MenuScreen.java
@@ -1,18 +1,73 @@
 package fi.tamk.tiko;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
-public class MenuScreen extends ParentScreen {
+public class MenuScreen implements Screen {
+    FoodChallenge game;
+    Texture background;
+    Rectangle play;
+    OrthographicCamera camera;
+
     MenuScreen (FoodChallenge game) {
-        super(game);
+        this.game = game;
+
+        background = new Texture("MainMenuScreen.png");
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 480, 800);
+
+        play = new Rectangle(48f, 480f, 304f, 64f);
+    }
+
+    @Override
+    public void show() {
+
     }
 
     @Override
     public void render(float delta) {
-        super.render(delta);
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.batch.draw(background, 0, 0, background.getWidth(), background.getHeight());
+        game.batch.end();
 
         checkPresses();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
     }
 
     private void checkPresses() {
@@ -22,9 +77,6 @@ public class MenuScreen extends ParentScreen {
 
             if (play.contains(touchPos.x, touchPos.y)) {
                 game.setScreen(new GameScreen(game));
-                dispose();
-            } else if (settings.contains(touchPos.x, touchPos.y)) {
-                game.setScreen(new SettingsScreen(game));
                 dispose();
             }
         }
