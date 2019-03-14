cc57046995ec37ecd31b73c9e17b35d6a539227a
Sami Bergman <sami.bergman@tuni.fi>
2019-03-14 15:07:20 +0200

    Cleaning code.

--- core/src/fi/tamk/tiko/FoodChallenge.java
+++ core/src/fi/tamk/tiko/FoodChallenge.java
@@ -1,29 +1,16 @@
 package fi.tamk.tiko;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 
 public class FoodChallenge extends Game {
 	SpriteBatch batch;
-	Texture img;
-	Rectangle playerRectangle;
-	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
-		MenuScreen mainmenu = new MenuScreen(this);
-        setScreen(mainmenu);
-
-		img = new Texture("SlimPlayerPH.png");
-		playerRectangle = new Rectangle(1,1, 1.5f, 1f);
-
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 10, 5);
+		setScreen(new MenuScreen(this));
 	}
 
 	@Override
@@ -49,7 +36,6 @@ public class FoodChallenge extends Game {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 
     public SpriteBatch getBatch() {
