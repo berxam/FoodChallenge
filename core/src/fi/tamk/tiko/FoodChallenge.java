93827a8a7d079fc469dfbaf3d6a3d86cf84aa780
Sami Bergman <sami.bergman@tuni.fi>
2019-03-14 14:59:20 +0200

    Cleaning code. Player scrolling down added.

--- core/src/fi/tamk/tiko/FoodChallenge.java
+++ core/src/fi/tamk/tiko/FoodChallenge.java
@@ -1,45 +1,29 @@
 package fi.tamk.tiko;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
 
 public class FoodChallenge extends Game {
 	SpriteBatch batch;
 	Texture img;
 	Rectangle playerRectangle;
 	OrthographicCamera camera;
-	//MenuScreen mainmenu;
-
-	public static final float worldWidth = 8f;
-	public static final float worldHeight = 4.8f;
-	public static final float pixelWidth = 800f;
-	public static final float pixelHeight = 480f;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
-
 		MenuScreen mainmenu = new MenuScreen(this);
-
-		//MenuScreen menuscreen = new MenuScreen(this);
-		//GameScreen gameScreen = new GameScreen(this);
         setScreen(mainmenu);
 
-
 		img = new Texture("SlimPlayerPH.png");
-		playerRectangle = new Rectangle(1,1,
-				1.5f, 1f);
+		playerRectangle = new Rectangle(1,1, 1.5f, 1f);
 
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 10, 5);
-
 	}
 
 	@Override
@@ -50,38 +34,6 @@ public class FoodChallenge extends Game {
 	@Override
 	public void render () {
 	    super.render();
-	    /*
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		batch.setProjectionMatrix(camera.combined);
-
-		batch.begin();
-		batch.draw(img, playerRectangle.x, playerRectangle.y, playerRectangle.getWidth(),
-				playerRectangle.getHeight());
-
-
-		if(Gdx.input.isTouched()) {
-			int realX = Gdx.input.getX();
-			int realY = Gdx.input.getY();
-
-			Vector3 touchPos = new Vector3(realX, realY, 0);
-
-			camera.unproject(touchPos);
-
-
-			playerRectangle.x = touchPos.x;
-			playerRectangle.y = touchPos.y;
-
-
-
-			//System.out.println("x = " + touchPos.x);
-			//System.out.println("y = " + touchPos.y);
-
-		}
-
-		batch.end();
-		*/
 	}
 
 	@Override
--- core/src/fi/tamk/tiko/GameScreen.java
+++ core/src/fi/tamk/tiko/GameScreen.java
@@ -5,85 +5,102 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-
 
 /**
- * if(inputIsTouched) ... ELSE {scrollaa ruudun mukana}
+ * Runs the game.
  */
 public class GameScreen implements Screen {
-  //  SpriteBatch batch;
-    FoodChallenge game;
-    Texture burger;
-    Texture carrot;
-    Texture slimPlayer;
-    Texture background;
-    OrthographicCamera camera;
-    Player player;
 
+    protected FoodChallenge game;
 
+    private Player player;
+    private Texture slimPlayer;
+    private Texture background;
+    private Texture burger;
+    private Texture carrot;
+    private OrthographicCamera camera;
+    private float bottomCounter = 0f; // How long is the player at the bottom?
+    private float scrollSpeed = 2f; // How fast does the screen scroll?
 
-
+    /**
+     * Creates camera, player and texture.
+     *
+     * @param game Don't even know what for but it must be there.
+     */
     GameScreen(FoodChallenge game) {
         this.game = game;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 400, 800);
         player = new Player();
         background = new Texture("tempbackground.jpg");
-
-
-
-
-
-
-    }
-
-    @Override
-    public void show() {
-
     }
 
     @Override
     public void render(float delta) {
+        clearScreen();
+        drawEverything();
+        movePlayer();
+    }
+
+    /**
+     * Clears the screen.
+     */
+    public void clearScreen() {
         Gdx.gl.glClearColor(1, 1, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //camera.update();
+    }
+
+    /**
+     * Sets projection matrix and draws textures.
+     */
+    public void drawEverything() {
         game.batch.setProjectionMatrix(camera.combined);
-       // stage.act(Gdx.graphics.getDeltaTime());
-       // stage.draw();
-      //  camera = new OrthographicCamera();
-       // camera.setToOrtho(false, 4.8f, 8);
 
         game.batch.begin();
-
-        game.batch.draw(background,0,0,800f,800f);
-
-        game.batch.draw(player.playerTexture,player.getPlayerX(),player.playerY,
-                player.getPlayerRectangle().getWidth(),player.getPlayerRectangle().getHeight());
-
-      //  batch.setProjectionMatrix(camera.combined);
-
-
-
+        game.batch.draw(background, 0, 0, 800f, 800f);
+        game.batch.draw(player.playerTexture,
+                player.getPlayerX(),player.playerY,
+                player.getPlayerRectangle().getWidth(),
+                player.getPlayerRectangle().getHeight());
         game.batch.end();
+    }
 
+    /**
+     * Moves player according to input.
+     *
+     * If no input is given, lets it move freely, but stops
+     * when hitting the bottom of the screen (1px padding).
+     */
+    public void movePlayer() {
         if (Gdx.input.isTouched()) {
             int realX = Gdx.input.getX();
             int realY = Gdx.input.getY();
 
             Vector3 touchPos = new Vector3(realX, realY, 0);
-
             camera.unproject(touchPos);
 
-            player.playerX = touchPos.x - 30f;
-            player.playerY = touchPos.y + 30f;
+            player.playerX = touchPos.x - 30f; // Positions the player
+            player.playerY = touchPos.y + 30f; // just above the finger.
 
+            bottomCounter = 0;
+        } else {
+            if (player.playerY > 2f) {
+                player.playerY -= scrollSpeed; // Lets the player fall down.
+            } else {
+                bottomCounter += 1; // Counts time while in the bottom.
+
+                if (bottomCounter == 100) {
+                    System.out.println("KESKITY ETTÄ SAAT HETELMIÄ");
+                } else if (bottomCounter == 200) {
+                    System.out.println("HEI PLIIS YRITÄ EDES");
+                }
+            }
         }
+    }
 
-
+    @Override
+    public void show() {
 
     }
 
